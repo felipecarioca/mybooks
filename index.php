77c5624ba138f1542a8b8c2bbf953b7959321bb3
Felipe Pereira <felipe.lfpm@gmail.com>
2019-12-09 17:39:04 -0300

    Ajuste no tamanhos das capas dos livros nos arquivos de CSS.

--- index.php
+++ index.php
@@ -179,7 +179,7 @@
 
                 <div class="row" v-if="typeof responseGoogleBooks !== 'undefined'">
 
-                  <div class="col-xl-2 col-md-6 mb-4" v-for="book, index of responseGoogleBooks.slice(0, 10)" v-if="Object.keys(book.volumeInfo).includes('imageLinks') && Object.keys(book.volumeInfo).includes('authors')">
+                  <div class="col-xl-3 col-md-6 mb-4" v-for="book, index of responseGoogleBooks.slice(0, 10)" v-if="Object.keys(book.volumeInfo).includes('imageLinks') && Object.keys(book.volumeInfo).includes('authors')">
                     <div class="card border-left-warning shadow h-100 py-2">
                       <div class="card-body">
                         <div class="row no-gutters">
@@ -232,8 +232,10 @@
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1" style="height: 32px;">{{livro.titulo}}</div>
-                        <div><img :src="livro.capa" alt="" title="" class="capa-livro"></div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{livro.paginas}} páginas</div>
+                        <div class="moldura-livro">
+                        	<img :src="livro.capa" alt="" title="" class="capa-livro">
+                        </div>
+                        <div class="h6 mb-0 font-weight-bold text-gray-800">{{livro.paginas}} páginas</div>
                       </div>
                     </div>
 
